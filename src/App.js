3aa5d62e90f207a74513d34303ba89fb317059c4
haniyehasadi <haniyeasadi627@gmail.com>
2023-08-10 23:39:15 +0330

    edit sidebar- add some items

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import './App.css';
 import Sidebar from './components/Sidebar';
-
 function App() {
   return (
     <Router>
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,13 +1,11 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
-import AppBar from '@mui/material/AppBar';
+// import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import List from '@mui/material/List';
 import Divider from '@mui/material/Divider';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import DashboardOutlinedIcon from '@mui/icons-material/DashboardOutlined';
 import AddBoxOutlinedIcon from '@mui/icons-material/AddBoxOutlined';
@@ -20,23 +18,20 @@ import ExpandLess from '@mui/icons-material/ExpandLess';
 import ExpandMore from '@mui/icons-material/ExpandMore';
 import Collapse from '@mui/material/Collapse';
 import IconButton from '@mui/material/IconButton';
-import axios from 'axios';
+import HeadsetOutlinedIcon from '@mui/icons-material/HeadsetOutlined';
+import HelpOutlineOutlinedIcon from '@mui/icons-material/HelpOutlineOutlined';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+// import Typography from '@mui/material/Typography';
+import { Link, useLocation } from 'react-router-dom';
 import './Sidebar.css';
 import '../App.css';
 
 const drawerWidth = 220;
 
-export default function PermanentDrawerLeft() {
-  React.useEffect(() => {
-    axios.get('https://assignment.abrnoc.com/regions')
-    .then((response) => {
-      console.log(response);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  }, []);
+const Sidebar = () =>{
   const [open, setOpen] = React.useState(false);
+  const location = useLocation();
   const handleOpen = () => {
     setOpen(!open);
   }
@@ -54,12 +49,9 @@ export default function PermanentDrawerLeft() {
         </Toolbar>
       </AppBar> */}
       <Drawer
-        // className="App-header"
-        // sx={{
-        //     width: drawerWidth
-        // }}
         sx={{
           width: drawerWidth,
+          height: '50vh',
           flexShrink: 0,
           '& .MuiDrawer-paper': {
             width: drawerWidth,
@@ -68,75 +60,150 @@ export default function PermanentDrawerLeft() {
         }}
         variant="permanent"
         anchor="left"
+        className='drawer-content'
       >
         <Toolbar>
-          <img className='logo' src="/cloudzy.png"></img>
+          <img className='logo' src="/cloudzy.png" alt="Cloudzy"></img>
         </Toolbar>
         <Divider />
         <List>
-            <ListItemButton className='listItems'>
-                <ListItem>
-                    <DashboardOutlinedIcon />
-                    <ListItemText primary={"Dashboard"} className='text'/>
-                </ListItem>
-            </ListItemButton>
-            <ListItemButton  className='listItems'>
-                <ListItem>
-                    <AddBoxOutlinedIcon />
-                    <ListItemText primary={"Instances"} className='text'/>
-                </ListItem>
-            </ListItemButton>
-            <ListItemButton className='listItems'>
-                <ListItem>
-                    <ContentCopyOutlinedIcon />
-                    <ListItemText primary={"Snapshots"} className='text'/>
-                </ListItem>
-            </ListItemButton>
-            <ListItemButton className='listItems'>
-                <ListItem>
-                    <VpnKeyOutlinedIcon />
-                    <ListItemText primary={"SSH Keys"} className='text'/>
-                </ListItem>
-            </ListItemButton>
+          <Link to="/dashboard" className='navigate'>
+            <Box 
+              className={location.pathname === "/dashboard" ? "sidebar-item clicked-item" : "sidebar-item"}
+            >
+              <Grid container spacing={2} className='grid-container '>
+                <Grid item md={2} sm={2} xs={2}>
+                  <DashboardOutlinedIcon />
+                </Grid>
+                <Grid item md={8} sm={8} xs={8}>
+                  Dashboard
+                </Grid>
+              </Grid>
+            </Box>
+          </Link>
+          <Link to="/instances" className='navigate'>
+            <Box 
+              className={location.pathname === "/instances" ? "sidebar-item clicked-item" : "sidebar-item"}
+            >
+              <Grid container spacing={2} className='grid-container '>
+                <Grid item md={2} sm={2} xs={2}>
+                  <AddBoxOutlinedIcon />
+                </Grid>
+                <Grid item md={8} sm={8} xs={8}>
+                  Instances
+                </Grid>
+              </Grid>
+            </Box>
+          </Link>
+          <Link to="/snapshots" className='navigate'>
+            <Box 
+              className={location.pathname === "/snapshots" ? "sidebar-item clicked-item" : "sidebar-item"}
+            >
+              <Grid container spacing={2} className='grid-container '>
+                <Grid item md={2} sm={2} xs={2}>
+                  <ContentCopyOutlinedIcon />
+                </Grid>
+                <Grid item md={8} sm={8} xs={8}>
+                  Snapshots
+                </Grid>
+              </Grid>
+            </Box>
+          </Link>
+          <Link to="/sshkeys" className='navigate'>
+            <Box 
+              className={location.pathname === "/sshkeys" ? "sidebar-item clicked-item" : "sidebar-item"}
+            >
+              <Grid container spacing={2} className='grid-container '>
+                <Grid item md={2} sm={2} xs={2}>
+                  <VpnKeyOutlinedIcon />
+                </Grid>
+                <Grid item md={8} sm={8} xs={8}>
+                  SSH Keys
+                </Grid>
+              </Grid>
+            </Box>
+          </Link>
         </List>
-        <Divider />
+        <Divider style={{marginTop: '50px'}}/>
         <List>
-            {/* <ListItemButton onClick={handleOpen} className='listItems'> */}
-                <ListItem className='listItems' >
-                    <AttachMoneyOutlinedIcon id="expandButtonItem" />
-                    <ListItemText primary={"Billing"} className='text'/>
-                    <IconButton onClick={handleOpen}>
-                      {open ? <ExpandLess /> : <ExpandMore />}
-                    </IconButton>
-                </ListItem>
-            {/* </ListItemButton> */}
-            {open ? (
-              <Collapse in={open} timeout="auto" unmountOnExit>
-                  <List disablePadding>
-                  <ListItemButton >
-                      <ListItemIcon>
-                          <AttachMoneyOutlinedIcon />
-                      </ListItemIcon>
-                      <ListItemText primary="Bill" />
-                  </ListItemButton>
-                  </List>
-              </Collapse>
-            ) : null
-            }
-            <ListItemButton className='listItems'>
-                <ListItem>
-                    <WifiOutlinedIcon />
-                    <ListItemText primary={"Networking"} className='text'/>
-                </ListItem>
-            </ListItemButton>
-            <ListItemButton className='listItems'>
-                <ListItem>
-                    <AssessmentOutlinedIcon />
-                    <ListItemText primary={"Report"} className='text'/>
-                </ListItem>
-            </ListItemButton>
+          <ListItem className='listItems' >
+            <AttachMoneyOutlinedIcon id="expandButtonItem" />
+            <ListItemText primary={"Billing"} className='text'/>
+            <IconButton onClick={handleOpen}>
+              {open ? <ExpandLess /> : <ExpandMore />}
+            </IconButton>
+          </ListItem>
+          {open ? (
+            <Collapse className='collapse-menu' in={open} timeout="auto" unmountOnExit >
+              <Link to="/bill" className='navigate'>
+                <Box 
+                  className={location.pathname === "/bill" ? "sidebar-item clicked-item" : "sidebar-item"}
+                >
+                  <Grid container spacing={2} className='grid-container '>
+                    <Grid item md={2} sm={2} xs={2}>
+                      <AttachMoneyOutlinedIcon />
+                    </Grid>
+                    <Grid item md={8} sm={8} xs={8}>
+                      Bill
+                    </Grid>
+                  </Grid>
+                </Box>
+              </Link>
+            </Collapse>
+          ) : null}
+          <Link to="/networking" className='navigate'>
+            <Box 
+              className={location.pathname === "/networking" ? "sidebar-item clicked-item" : "sidebar-item"}
+            >
+              <Grid container spacing={2} className='grid-container '>
+                <Grid item md={2} sm={2} xs={2}>
+                  <WifiOutlinedIcon />
+                </Grid>
+                <Grid item md={8} sm={8} xs={8}>
+                  Networking
+                </Grid>
+              </Grid>
+            </Box>
+          </Link>
+          <Link to="/report" className='navigate'>
+            <Box 
+              className={location.pathname === "/report" ? "sidebar-item clicked-item" : "sidebar-item"}
+            >
+              <Grid container spacing={2} className='grid-container '>
+                <Grid item md={2} sm={2} xs={2}>
+                  <AssessmentOutlinedIcon />
+                </Grid>
+                <Grid item md={8} sm={8} xs={8}>
+                  Report
+                </Grid>
+              </Grid>
+            </Box>
+          </Link>
         </List>
+        <div className="sidebar-boxes">
+          <Box className="box">
+            <Grid container spacing={2} className='grid-container'>
+              <Grid item md={2} sm={2} xs={2}>
+                <HeadsetOutlinedIcon />
+              </Grid>
+              <Grid item md={7} sm={8} xs={8}>
+                Support
+              </Grid>
+            </Grid>
+          </Box>
+          <Box className="box">
+            <Grid container spacing={2} className='grid-container'>
+              <Grid item md={2} sm={2} xs={2}>
+                <HelpOutlineOutlinedIcon />
+              </Grid>
+              <Grid item md={7} sm={8} xs={8}>
+                Help
+              </Grid>
+            </Grid>
+          </Box>
+        </div>
       </Drawer>
     </Box>
   );
 }
+export default Sidebar;
